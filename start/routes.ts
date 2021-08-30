d158222bba784628c286ce54d9fd1daf77946198
Danang Ponorogo <danang.dandies@gmail.com>
2021-08-30 14:26:50 +0700

    Fix on app.edge

--- start/routes.ts
+++ start/routes.ts
@@ -20,10 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
-
-Route.get('/test', async ({ inertia }) => {
+Route.get('/', async ({ inertia }) => {
   return inertia.render('Test', { nama: 'Danang Dismantoro' })
 })
